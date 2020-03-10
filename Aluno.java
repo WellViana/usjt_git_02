c5efc61bf691927309f1789838c06ee68b4b5574
Alunos <Alunos@BUTLI0426.animaedu.intranet>
2020-03-10 20:50:02 -0300

    creating setters

--- Aluno.java
+++ Aluno.java
@@ -1,5 +1,12 @@
 public class Aluno {
   private String nome;
   private int id;
-
+  
+  public void setNome(String nome) {
+     this.nome = nome;
+  }
+  
+  public void setId(int id) {
+     this.id = id;
+  }
 }
