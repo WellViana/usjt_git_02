786104d60219d52a7556336161369ee95f53074b
WellsSA <wel.cavzod@gmail.com>
2020-03-10 21:06:13 -0300

    removing getters and setters

--- Aluno.java
+++ Aluno.java
@@ -1,25 +1,9 @@
 public class Aluno {
   private String nome;
   private int id;
-   
+
   public Aluno(int id, String nome) {
     this.id = id;
-    this.nome = nome;
-  }
-
-  public int getId() {
-    return this.id;
-  }
-
-  public String getNome() {
-    return this.nome;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public void setNome(String nome) {
     this.nome = nome;
   }
 
